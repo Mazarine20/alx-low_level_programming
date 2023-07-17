4743f34b48a7e33828205bdaaae9a14d0af43bb6
Mazarine SANVI <mazarinesanvi@gmail.com>
2023-07-17 12:29:07 -0700

    c programming

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void) 
+{
     printf("Size of char: %zu byte(s)\n", sizeof(char));
     printf("Size of int: %zu byte(s)\n", sizeof(int));
     printf("Size of float: %zu byte(s)\n", sizeof(float));
