86e39161e3a32b44be5c185a39f30c25ed0db382
Mazarine SANVI <mazarinesanvi@gmail.com>
2023-07-21 06:39:48 -0700

    function

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,10 +2,18 @@
 
 /**
  * _isupper - checks for uppercase character
- * @c: the character to be checked
+ * @c: character to check
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+    if (c >= 'A' && c <= 'Z')
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
 }
