c16d2474c075b88ea1dd3d9dd5f368fdceea5454
Mazarine SANVI <mazarinesanvi@gmail.com>
2023-07-21 04:27:42 -0700

    boucle if

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,16 +1,17 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - tests function that prints if integer is positive or negative
- * Return: 0
- */
-int main(void)
-{
-	int i;
+#include <stdio.h>
 
-	i = 0;
 
-	positive_or_negative(i);
+void positive_or_negative(int i);
 
-	return (0);
-}
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
+
+
+
+#endif /* MAIN_H */
